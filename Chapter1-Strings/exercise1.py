df7d243874efa1bb3386952484bf17fbe2264a44
thmjiya022 <mjiyakhothobani@gmail.com>
2024-11-29 13:44:19 +0200

    added exercise1 instructions

---
+++ Chapter1-Strings/exercise1.py
@@ -0,0 +1,16 @@
+'''
+Exercise 1- Number guessing game
+
+For this exercise:
+- Write a function (guessing_game) that takes no arguments.
+- When run, the function chooses a random integer between 0 and 100 (inclusive).
+- Then ask the user to guess what number has been chosen.
+- Each time the user enters a guess, the program indicates one of the following:
+    - Too high
+    - Too low
+    - Just right
+- If the user guesses correctly, the program exits. Otherwirse, ghe user is asked to try again
+- The program only exists after the user guesses correctly
+'''
+
+
