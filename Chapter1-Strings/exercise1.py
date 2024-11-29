97866fd38abd0b12b5312ce1f04ea4a28b522a17
thmjiya022 <mjiyakhothobani@gmail.com>
2024-11-29 14:00:17 +0200

    added solution for exercise1

--- Chapter1-Strings/exercise1.py
+++ Chapter1-Strings/exercise1.py
@@ -13,4 +13,23 @@ For this exercise:
 - The program only exists after the user guesses correctly
 '''
 
+import random
 
+def guessing_game():
+    answer = random.randint(0, 100)
+
+    while True:
+        user_guess = int(input ('Whats is your guess? '))
+
+        if user_guess == answer:
+            print(f'Right! The answer is {user_guess}')
+            break
+
+        if user_guess < answer:
+            print(f'Your guess of {user_guess} is too low!')
+
+        else:
+            print(f'Your guess of {user_guess} is too high!')
+
+guessing_game()
+            
