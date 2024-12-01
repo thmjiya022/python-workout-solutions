4e9ebe1b218fc3e10ae38de3546782a1cde89dc4
thmjiya022 <mjiyakhothobani@gmail.com>
2024-12-01 22:46:21 +0200

    exercise problem description

---
+++ Chapter1-Strings/exercise2.py
@@ -0,0 +1,12 @@
+'''
+Exercise 2 - Summing numbers
+
+For this exercise:
+- Write a function (`mysum`) that takes a variable number of arguments.
+- The function should compute the sum of all the provided arguments.
+- If no arguments are provided, the function should return 0.
+- Example usage and expected outputs are shown below:
+    - mysum(1, 2, 3) -> 6
+    - mysum(10, 20, 30, 40, 50) -> 150
+    - mysum() -> 0
+'''
