b80d116fa7087e8bd2f140ca38726c7aaa8f00c4
thmjiya022 <mjiyakhothobani@gmail.com>
2024-12-02 21:14:04 +0200

    added instructions for exercise3

---
+++ Chapter1-Strings/exercise3.py
@@ -0,0 +1,23 @@
+'''
+Exercise 3 - Run Timing
+For this exercise:
+
+Write a function (run_timing) that tracks and calculates the average time for a 10 km run.
+The function should repeatedly prompt the user to enter the time (in minutes) it took to complete a 10 km run.
+The user can stop entering data by pressing Enter without typing a number.
+Once the user stops entering data, the function should:
+Calculate the average time of all entered run times.
+Display the average as a floating-point number and the total number of runs.
+Example usage and expected outputs are shown below:
+
+Input:
+
+Enter 10 km run time: 15
+Enter 10 km run time: 20
+Enter 10 km run time: 10
+Enter 10 km run time: 
+
+Output:
+
+Average of 15.0, over 3 runs
+'''
