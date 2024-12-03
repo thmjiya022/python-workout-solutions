d965d868035681988b12d34086a964b5c3803571
thmjiya022 <mjiyakhothobani@gmail.com>
2024-12-03 14:27:22 +0200

    added solution for exercise3

--- Chapter1-Strings/exercise3.py
+++ Chapter1-Strings/exercise3.py
@@ -21,3 +21,22 @@ Output:
 
 Average of 15.0, over 3 runs
 '''
+
+def run_timing():
+    number_of_runs = 0
+    total_time = 0
+
+    while True:
+        one_run = input('Enter 10 km run time: ')
+
+        if not one_run:
+            break
+
+        number_of_runs ++ 1
+        total_time += float(one_run)
+
+    average_time = total_time/ number_of_runs
+
+    print(f'Average of {average_time}, over {number_of_runs} runs')
+
+run_timing()
